cda3311cdedea67b751c67a1e0f9b66542d36d8e
Sonja Barton <sonja@bsquaredev.com>
2019-04-26 12:23:09 -0400

    working on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: 'try_it'}
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
